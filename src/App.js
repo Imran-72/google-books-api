f94ffd2f6fbea2ad0eff7d340dd8c126eae55470
Imran Abdurakhmanov <petrow318@gmail.com>
2021-11-20 11:02:38 +0300

    установил axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [startIndex, setStartIndex] = useState(1);
+  const [maxResults, setMaxResalts] = useState(10);
+  const [query, setQuery] = useState("");
+  const [cards, setCards] = useState([]);
+
+  console.log(cards);
+
+  const handleSubmit = () => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+      )
+      .then((response) => setCards(response.data.items));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <input
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button onClick={handleSubmit}>поиск</button>
+      </div>
+      <div>
+        {cards &&
+          cards.map((item) => (
+            <img src={item.volumeInfo.imageLinks.thumbnail} />
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
