2d35daadfe5dbad3ddd36f5b3bf6b88dd411238d
Imran Abdurakhmanov <petrow318@gmail.com>
2021-11-22 18:39:22 +0300

    сделал пагинацию с использованием lodash

--- src/app/components/bookCard.jsx
+++ src/app/components/bookCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BookCard = ({ item }) => {
   return (
     <div className="m-2">
-      <img src={item.volumeInfo.imageLinks.thumbnail} alt="Обложка книги" />
+      <img src={item} alt="Обложка книги" />
     </div>
   );
 };
